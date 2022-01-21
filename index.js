611db5063a582aa2e4ff5b4300daf822bc2b7258
Di IbragUbuntu <ibragimov.dinar2015@yandex.ru>
2022-01-21 14:53:44 +0300

    Init new API on NodeJS

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-console.log('Express started');
 import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
+import fileUpload from "express-fileupload";
 
 const PORT = 5000;
+const DB_URL = `mongodb+srv://Znamenie:Znamenie1@cluster0.kamwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json('Server worked')
-})
+app.use(express.json());
+app.use(express.static('static'));
+app.use(fileUpload({}));
+app.use('/api', router);
 
-app.listen(PORT, () => console.log('SERVER STARTED IN PORT' + PORT));
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => console.log('SERVER STARTED IN PORT' + PORT));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+startApp();

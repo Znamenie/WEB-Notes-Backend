d61e1bb8033c1d6e227a0498232e7c9d8afe7bdb
Di IbragUbuntu <ibragimov.dinar2015@yandex.ru>
2022-01-21 14:54:04 +0300

    Change router

--- router.js
+++ router.js
@@ -2,10 +2,4 @@ import { Router } from "express";
 
 const router = new Router();
 
-// router.post('/posts', PostController.create)
-// router.get('/posts', PostController.getAll)
-// router.get('/posts/:id', PostController.getOne)
-// router.put('/posts', PostController.update)
-// router.delete('/posts/:id', PostController.delete)
-
 export default router;
